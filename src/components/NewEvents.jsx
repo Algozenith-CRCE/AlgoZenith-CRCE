2a55dce3bfd0ec5662ca770ace7619ca6b5c0e15
Alroy <alroypereira05@gmail.com>
2023-09-21 12:44:07 +0530

    updated events poster

--- src/components/NewEvents.jsx
+++ src/components/NewEvents.jsx
@@ -3,8 +3,7 @@ import { React } from 'react'
 import './NewEvents.css';
 import { Link } from 'react-router-dom';
 import Fade from 'react-reveal/Fade';
-import comingSoon from '../assets/comingSoon.png'
-// import newEvent from '../assets/suspenseStory.mp4'
+import ss from '../assets/ss.png'
 import Lottie from 'lottie-react';
 import Timer from '../assets/comingAnime.json'
 
@@ -14,7 +13,7 @@ export default function newEvents(props) {
       <div className='wrap-register' style={{'marginTop':props.margin}}>
         <Fade bottom>
           <div className='poster-container'>
-            <img src={comingSoon} alt="Event-Poster" className='event-poster'/>
+            <img src={ss} alt="Event-Poster" className='event-poster'/>
           </div>
           <div className='register-div'>
             <Lottie animationData={Timer} className='coming-image'/>
