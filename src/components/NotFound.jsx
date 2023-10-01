156d0dbf6fe68d66c8690313b440d161bcfcc2d2
Alroy <alroypereira05@gmail.com>
2023-10-01 18:21:38 +0530

    fixed the logo

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -3,10 +3,12 @@ import Lottie from 'lottie-react';
 import notFound from '../assets/404.json';
 import './NotFound.css';
 import { Link } from 'react-router-dom';
+import blueLogo from '../assets/blueLogo.svg'
 
 function NotFound() {
   return (
     <div className='notfound-container'>
+    <img src={blueLogo} className='nf-logo'/>
       <Lottie animationData={notFound} />
       <p>This is not the web page you are looking for.</p>
       <Link to="/">
